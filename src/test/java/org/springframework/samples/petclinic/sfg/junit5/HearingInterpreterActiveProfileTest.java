1e36ebff04e84c3f60ec8239baa2ff8a5cd54e0a
marianos <marianos8125@gmail.com>
2021-07-12 10:25:20 -0300

    add active profile

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterActiveProfileTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterActiveProfileTest.java
@@ -2,17 +2,23 @@ package org.springframework.samples.petclinic.sfg.junit5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
-import org.springframework.samples.petclinic.sfg.LaurelConfig;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ActiveProfiles("base-test")
-@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
-public class HearingInterpreterLaurelTest {
+@ActiveProfiles("yanny")
+@SpringJUnitConfig(classes = HearingInterpreterActiveProfileTest.TestConfig.class)
+public class HearingInterpreterActiveProfileTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig {
+
+    }
 
     @Autowired
     HearingInterpreter hearingInterpreter;
@@ -21,6 +27,6 @@ public class HearingInterpreterLaurelTest {
     void whatIheard() {
         String word = hearingInterpreter.whatIheard();
 
-        assertEquals("Laurel", word);
+        assertEquals("Yanny", word);
     }
 }
